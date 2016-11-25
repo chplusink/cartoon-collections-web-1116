9affd3abb9274b8cc75f1188c08dfd6bff02a92d
Vic Lam <lam.vic.k@gmail.com>
2016-11-25 22:22:42 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(formacion)
+  formacion.each_with_index {|enano,indice| puts "#{indice+1} #{enano}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(formacion)
+  formacion.collect {|planeteer| planeteer.capitalize<<"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(formacion)
+  formacion.any? {|palabra| palabra.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(formacion)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  formacion.find {|ingrediente| cheese_types.include?(ingrediente)}
 end
